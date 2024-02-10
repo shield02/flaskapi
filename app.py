4e72ddcdb38a1b04318d30ecf088d1a0c6cddfe5
Otuekong Etim <myshield02@gmail.com>
2024-02-10 17:29:35 +0100

    added a list function to all filters

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_tasks():
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 @auth.login_required
 def get_task(task_id):
-    task = filter(lambda task: task['id'] == task_id, tasks)
+    task = list(filter(lambda task: task['id'] == task_id, tasks))
     if len(task) == 0:
         abort(404)
     return jsonify({'task': make_public_task(task[0])})
@@ -78,7 +78,7 @@ def create_task():
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 @auth.login_required
 def update_task(task_id):
-    task = filter(lambda task: task['id'] == task_id, tasks)
+    task = list(filter(lambda task: task['id'] == task_id, tasks))
     if len(task) == 0:
         abort(404)
     if not request.json:
@@ -97,7 +97,7 @@ def update_task(task_id):
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 @auth.login_required
 def delete_task(task_id):
-    task = filter(lambda task: task['id'] == task_id, tasks)
+    task = list(filter(lambda task: task['id'] == task_id))
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
