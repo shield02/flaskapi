d826ce6797e3a5feba13c8d5aee4346345f9d6c0
Otuekong Etim <myshield02@gmail.com>
2024-02-10 17:25:27 +0100

    code refactoring to all the functions

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ auth = HTTPBasicAuth()
 def get_password(username):
     if username == 'king':
         return 'correct'
-    else:
-        return None
+    return None
     
 @auth.error_handler
 def unauthorized():
@@ -52,16 +51,18 @@ def make_public_task(task):
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 @auth.login_required
 def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+    return jsonify({'tasks': list(map(make_public_task, tasks))})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
 def get_task(task_id):
-    task = [make_public_task(task) for task in tasks if task['id'] == task_id]
+    task = filter(lambda task: task['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
 def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -75,26 +76,28 @@ def create_task():
     return jsonify({'task': make_public_task(task)}), 201
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
 def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = filter(lambda task: task['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != str:
+    if 'title' in request.json and type(request.json['title']) != unicode:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not str:
+    if 'description' in request.json and type(request.json['description']) is not unicode:
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@auth.login_required
 def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
+    task = filter(lambda task: task['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
